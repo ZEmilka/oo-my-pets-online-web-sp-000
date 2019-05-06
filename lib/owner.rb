db84a2476184cb91788545745cb4f61ec2e79664
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-06 18:01:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,57 +5,57 @@ class Owner
 
   @@all=[]
 
-  def intitialize(species)
+   def intitialize(species)
     @species=species
     @@all<<self
     @@pets={:cats=>[],:dogs=>[],:fishes=>[]}
-  end
+   end
 
-  def self.all
+   def self.all
     @@all
-  end
+   end
 
-  def self.count
+   def self.count
     @@all.size
-  end
+   end
 
-  def self.reset_all
+   def self.reset_all
     @@all.clear
-  end
+   end
 
-  def say_species
+   def say_species
     "I am a #{@species}."
-  end
+   end
 
-  def buy_cat(cat)
+   def buy_cat(cat)
     @@pets[:cats]<<Cat.new(cat)
-  end
+   end
 
-  def buy_dog(dog)
+   def buy_dog(dog)
     @@pets[:dogs]<<Dog.new(dog)
-  end
+   end
 
-  def buy_fish(fish)
+   def buy_fish(fish)
     @@pets[:fishes]<<Fish.new(fish)
-  end
+   end
 
-  def walk_dogs
+   def walk_dogs
     @pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
-  end
+   end
 
-  def play_with_cats
+   def play_with_cats
     @pets[:cats].each do |cat|
       cat.mood = "happy"
     end
-  end
+   end
 
-  def feed_fish
+   def feed_fish
     @pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
-  end
+   end
 
    def sell_pets
      @pets.collect do |species, instances|
